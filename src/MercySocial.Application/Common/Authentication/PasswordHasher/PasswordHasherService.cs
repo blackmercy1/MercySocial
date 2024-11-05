0a78975b1be7c7263089837e3a11b8769eee9a5d
blackmercy <leshaar14@gmail.com>
2024-11-05 07:55:54 +0400

    Added bcrypt hashing into application layer

--- src/MercySocial.Application/Common/Authentication/PasswordHasher/PasswordHasherService.cs
+++ src/MercySocial.Application/Common/Authentication/PasswordHasher/PasswordHasherService.cs
@@ -1,23 +1,8 @@
-using System.Security.Cryptography;
-using System.Text;
-
 namespace MercySocial.Application.Common.Authentication.PasswordHasher;
 
 public class PasswordHasherService : IPasswordHasherService
 {
-    public string HashPassword(string password)
-    {
-        using var sha256 = SHA256.Create();
-        
-        var passwordBytes = Encoding.UTF8.GetBytes(password);
-        var hashedBytes = sha256.ComputeHash(passwordBytes);
-
-        return Convert.ToBase64String(hashedBytes);
-    }
-
-    public bool VerifyPassword(string hashedPassword, string providedPassword)
-    {
-        var hashedProvidedPassword = HashPassword(providedPassword);
-        return hashedPassword == hashedProvidedPassword;
-    }
+    public string HashPassword(string password) => BCrypt.Net.BCrypt.HashPassword(password);
+    public bool VerifyPassword(string hashedPassword, string providedPassword) 
+        => hashedPassword == BCrypt.Net.BCrypt.HashPassword(providedPassword);
 }
