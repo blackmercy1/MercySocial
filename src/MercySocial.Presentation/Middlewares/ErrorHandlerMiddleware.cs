d4a2448a18c1dac5c1594ffd6b08c26b696cfef8
black_mercy <leshaar14@gmail.com>
2024-11-23 15:28:13 +0400

    fix: Uncommite code in ErrorHandlerMiddleware

--- src/MercySocial.Presentation/Middlewares/ErrorHandlerMiddleware.cs
+++ src/MercySocial.Presentation/Middlewares/ErrorHandlerMiddleware.cs
@@ -17,16 +17,14 @@ public class ErrorHandlerMiddleware : IMiddleware
         }
         catch (Exception exception)
         {
-            // const string message = "An unhandled exception has occurred while executing the request.";
-            // _logger.LogError(exception, message);
-            //
-            // context.Response.Clear();
-            // context.Response.StatusCode = StatusCodes.Status500InternalServerError;
+            const string message = "An unhandled exception occurred while processing the request.";
+            _logger.LogError(exception, message);
             
+            context.Response.Clear();
             context.Response.ContentType = "application/json";
             context.Response.StatusCode = StatusCodes.Status500InternalServerError;
 
-            var result = new { error = exception.Message };
+            var result = new { error = "An error occurred while processing your request. Please try again later." };
             await context.Response.WriteAsJsonAsync(result);
         }
     }
